5cf7de62802447ceb603ce6d28f21837200ecd78
yogapratrian <yogapratrian@gmail.com>
2026-03-03 20:17:03 +0800

    Commit

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('landing');
 });
+
+Route::get('/landing', [App\Http\Controllers\LandingController::class, 'index'])->name('landing');
