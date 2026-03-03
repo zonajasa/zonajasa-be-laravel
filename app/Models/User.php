5fa825b0bd0db9ef3ef9aa1ea4a2b33c7ef80473
dev-xamppsul <muhdevapp@gmail.com>
2026-03-04 06:58:31 +0800

    fix library auth with passport

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,11 +6,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Passport\Contracts\OAuthenticatable;
+use Laravel\Passport\HasApiTokens;
 
-class User extends Authenticatable
+class User extends Authenticatable implements OAuthenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
--- routes/api.php
+++ routes/api.php
@@ -1,14 +1,48 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Laravel\Passport\Token;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::get('/user-session', function (Request $request) {
+    return $request->user(); //get user session
+})->middleware('auth:api');
 
-Route::get('/test', function () {
-    return response()->json([
-        'message' => 'Hello, World!',
-    ]);
+Route::get('/test-user-token', function (Request $request) {
+
+    //validation request
+    $req = $request->post('user_id');
+    if (empty($req)) {
+        return response()->json([
+            'message' => 'User id is required'
+        ], 422);
+    }
+
+    //get reterive user data by id valid or not
+    $user = User::find($req);
+    if (!empty($user)) {
+
+        //create token personal akses
+        $token = $user->createToken('zonajasa')->accessToken;
+
+        //retriving token valid as user
+        $tokenValid = $user->tokens()
+            ->with('client')
+            ->where('revoked', false)
+            ->where('expires_at', '>', now())
+            ->get()
+            ->filter(fn(Token $token) => $token->client->hasGrantType('personal_access'));
+        return response()->json([
+            'message' => 'User found',
+            'user' => $user,
+            'token' => $token,
+            'token_valid' => $tokenValid,
+        ], 200);
+    } else {
+        //default invalid user id
+        return response()->json([
+            'message' => 'User not found'
+        ], 422);
+    }
 });
