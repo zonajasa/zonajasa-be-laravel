c50c423642f6859f9c8af4772d6f6497d4873f1b
yogapratrian <yogapratrian@gmail.com>
2026-03-05 10:09:56 +0800

    Merge: web.php fix

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,4 @@
 <?php
 
-<<<<<<< HEAD
-use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('home');
-})->name('home');
-=======
 #import landing routes
 require base_path('app/Internal/Web/Landing/Routes/LandingRoutes.php');
->>>>>>> authentication
