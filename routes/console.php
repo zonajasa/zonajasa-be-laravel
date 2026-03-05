255a7f4630a3420997ff9b89ea08e980df28b2f7
dev-xamppsul <muhdevapp@gmail.com>
2026-03-05 11:40:31 +0800

    job: schedule check otp expire

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,13 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Schedule::command('otp:check-expire')
+    ->everyThirtySeconds() //after 30 seconds
+    ->withoutOverlapping(10) //under 10 seconds for timeout
+    ->name('otp-check-expire');
