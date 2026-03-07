f2d580cd82a8b133c07407e7ef7683a21bb5e7e4
dev-xamppsul <muhdevapp@gmail.com>
2026-03-07 11:46:46 +0800

    services .env:use string and value yes

--- config/services.php
+++ config/services.php
@@ -38,7 +38,7 @@ return [
     'waha' => [
         'api_key' => env('WAHA_API_KEY'),
         'accept' => env('WAHA_ACCEPT'),
-        'api_base_url' => env('CONTAINER_STATUS') === 'true' ? env('WAHA_BASE_URL') : env('WAHA_API_BASE_URL'),
+        'api_base_url' => env('CONTAINER_STATUS') == "yes" ? env('WAHA_BASE_URL') : env('WAHA_API_BASE_URL'),
     ],
 
 ];
